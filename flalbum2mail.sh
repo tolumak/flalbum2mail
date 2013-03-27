25af334d7ec6f9acf10f9693503e67e39f9fe9ed
Michel Lafon-Puyo <michel.lafonpuyo@gmail.com>
2013-03-27 13:24:30 +0100

    flalbum2mail.sh: split photo cells on multiple HTMl line

--- flalbum2mail.sh
+++ flalbum2mail.sh
@@ -3,10 +3,8 @@
 ALBUM_NAME="$1"
 ALBUM_URL="$2"
 AUTH="$3"
-FROM="$4"
-FROM_NAME="$5"
 
-shift 5
+shift 3 
 
 photos=`./flalbum2mail --album "$ALBUM_NAME" --auth "$AUTH" | grep -e "^A" | cut -d ' ' -f 3-4 --output-delimiter=','`
 
@@ -15,14 +13,17 @@ for i in $photos ; do
     photo_count=$(( $photo_count + 1 ))
     page=`echo $i | cut -d ',' -f 1`
     img=`echo $i | cut -d ',' -f 2`
-    photo_list_as_cells="${photo_list_as_cells}<td><a href=$page><img src=$img></a></td>"
+    photo_list_as_cells="${photo_list_as_cells}<td><a href=$page><img src=$img></a></td>\n"
 done
 
 sed_script="sed -e 's/album_name/$ALBUM_NAME/g' -e 's/photo_count/$photo_count/g' -e 's#photo_list_as_cells#$photo_list_as_cells#g' -e 's#album_url#$ALBUM_URL#g'"
 
-message=`cat message | eval $sed_script`
-subject=`cat subject | eval $sed_script`
+rm -f "$ALBUM_NAME.msg"
+if [ $photo_count -gt 0 ] ; then
+    cat album.tmpl | eval $sed_script > "$ALBUM_NAME.msg"
+fi
 
+exit 0
 if [ $photo_count -gt 0 ] ; then
     echo $message | mailx $* -s "`echo $subject`" -a 'Content-Type: text/html' -- -F "$FROM_NAME" -r "$FROM"
 fi
